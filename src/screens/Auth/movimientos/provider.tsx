048481dcf9f12a87d1a0604b3d5addb4d921cc4e
Derlis Ruiz Diaz <derlisruizdiaz@Derliss-MacBook-Pro.local>
2023-11-22 22:20:54 -0300

    agregando tipos a movimientos

--- src/screens/Auth/movimientos/provider.tsx
+++ src/screens/Auth/movimientos/provider.tsx
@@ -1,26 +1,56 @@
 import { ReactNode, createContext,useContext,useState } from "react";
+import { APICALLER } from "../../../services/api";
+import { useAuthProvider } from "../../../providers/authprovider";
 
 
+type fechaForm = {
+    inicio: string
+    fin: string
+}
 
 
+type MovimentosProps = {
+    movimientos: never[],
+    loading: boolean,
+    setFecha: React.Dispatch<React.SetStateAction<fechaForm>>
+    getMovimientos:()=>void
+} 
 
-const MovimientosContext = createContext({
-    movimientos: []
+const MovimientosContext = createContext<MovimentosProps>({
+    movimientos: [],
+    loading: false,
+    setFecha: ()=>{},
+    getMovimientos: ()=>{}
 })
 
 
 function MovimientosProvider({children}:{children : ReactNode}) {
-
+    const {userData} = useAuthProvider()
+    const [fecha,setFecha] = useState({
+        inicio:'',
+        fin:''
+    })
     const [movimientos,setMovimientos] = useState([])
+    const [loading,setLoading] = useState(false)
 
-    const values = {movimientos}
+    const getMovimientos = async()=>{
+        setLoading(true)
+        const res = await APICALLER.get({url:`movimientos?fecha_inicio=${fecha.inicio}&fecha_fin=${fecha.fin}`,token:userData.token})
+        console.log(res);
+        if(res.success){
+            setMovimientos(res.results)
+        }
+        setLoading(false)
+    }
+
+    const values = {movimientos,setFecha,loading, getMovimientos}
 
     return ( <MovimientosContext.Provider value={values}>{children}</MovimientosContext.Provider> );
 }
 
 export const useMovimientos = ()=>{
-    const {movimientos} = useContext(MovimientosContext)
-    return {movimientos}
+    const {movimientos,setFecha,loading, getMovimientos} = useContext(MovimientosContext)
+    return {movimientos,setFecha,loading, getMovimientos}
 }
 
 export default MovimientosProvider;
